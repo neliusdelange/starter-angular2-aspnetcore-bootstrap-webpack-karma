f2c7054345ae525df3e802a843402e3e3e499579
neliusdelange <lias@thenarrow.com>
2016-11-03 06:06:26 +0000

    Initial

---
+++ src/AngNetCore/src/app/app.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap-theme.css';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent { }
